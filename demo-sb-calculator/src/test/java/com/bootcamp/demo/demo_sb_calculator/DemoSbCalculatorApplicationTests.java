11bbc7fecdbad5d3e9dae9fb0774ee72683d53a7
stepauzi <stepauzi@ymail.com>
2025-09-23 12:02:12 +0800

    update

---
+++ demo-sb-calculator/src/test/java/com/bootcamp/demo/demo_sb_calculator/DemoSbCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.demo.demo_sb_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSbCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
